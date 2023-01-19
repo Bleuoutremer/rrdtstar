4716f58e0da87c1682b9c7492cc4e0f59f6aee29
Jing Zhang <37541764+Bleuoutremer@users.noreply.github.com>
2023-01-19 12:07:02 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,121 @@
+
+| <ins>__ARCHIVE NOTICE__</ins>: The implemneted environment had been cleaned up and extended into a dedicated SBP planning module at [sbp-env](https://github.com/soraxas/sbp-env). This repo will remains as is for the ICRA'19 version. |
+| ------------------------------------------------------------------------------ |
+
+# Rapidly-exploring Random disjointed-Trees
+
+RRdT* uses multiple disjointed-trees to exploit local-connectivity of spaces via [Markov Chain Monte Carlo (MCMC)](https://en.wikipedia.org/wiki/Markov_chain_Monte_Carlo) random walk, which utilises neighbourhood information derived from previous successful and failed samples. The active balancing of global exploration and local exploitation is the key to improve sample efficiency.
+
+This is a repository that contains a Python implementation of the algorithm describe in the paper, along with implementation of other state-of-the-art planners that had been experimentally compared to RRdT*.
+
+The algorithm is described in the [paper](https://arxiv.org/abs/1810.03749) from ICRA'19. A complementary video that explains the concept can be found [here](https://youtu.be/Cy-kYrzkPvA). If you want to cite this work in your research, you can use [this BibTex entry](#bibtex).
+
+
+## Installation
+
+#### Optional
+
+I always recommend people to create a virtual environment for each of their specific research project. You can do this with
+```sh
+# assuming python3 and bash shell
+python -m venv rrdt
+source rrdt/bin/activate
+```
+to create a virtual environment named as `rrdt`.
+
+#### Install dependencies
+
+You can install all the needed packages with pip.
+```sh
+pip install -r requirements.txt
+```
+
+
+## Quick Guide
+
+You can get a detailed help message with
+```sh
+python main.py --help
+```
+but the basic syntax is
+```sh
+python main.py <PLANNER> <MAP> [options]
+```
+It will open a new window that display a map on it. Every white pixel is assumed to be free, and non-white pixels are obstacles. You will need to use your mouse to select two points on the map, the first will be set as the starting point and the second as the goal point.
+
+## Demos
+
+### Run maps with different available Planners
+
+This repository contains a framework to performs quick experiments for Sampling-Based Planners (SBPs) that are implemented in Python. The followings are planners that had implemented and experimented in this framework.
+
+Note that the commands shown in the respective demos can be customised with additional options. In fact, the actual command format used for the demonstrations is
+```sh
+python main.py <PLANNER> maps/room1.png start <sx> <sy> goal <sx> <sy> -vv
+```
+to have a fix set of starting and goal points for consistent visualisation, but we omitted the start/goal options in the following commands for clarity.
+
+### RRdT*
+
+```sh
+python main.py rrdt maps/room1.png -vv
+```
+
+<p align="center">
+    <img width="600" height="auto" src="doc/images/rrdt.gif" alt="RRdT* Planner" />
+</p>
+
+### RRT*
+
+```sh
+python main.py rrt maps/room1.png -vv
+```
+
+<p align="center">
+    <img width="600" height="auto" src="doc/images/rrt.gif" alt="RRT* Planner" />
+</p>
+
+### Bi-RRT*
+
+```sh
+python main.py birrt maps/room1.png -vv
+```
+
+<p align="center">
+    <img width="600" height="auto" src="doc/images/birrt.gif" alt="Bi-RRT* Planner" />
+</p>
+
+### Informed RRT*
+
+```sh
+python main.py informedrrt maps/room1.png -vv
+```
+
+<p align="center">
+<img width="600" height="auto" src="doc/images/informedrrt.gif" alt="Informed RRT* Planner" />
+</p>
+
+The red ellipse shown is the dynamic sampling area for Informed RRT*
+
+### Others
+
+There are also some other planners included in this repository. Some are preliminary planner that inspired RRdT*, some are planners with preliminary ideas, and some are useful for debugging.
+
+## How does it works
+
+You can have a look of the paper *[Balancing Global Exploration and Local-connectivity Exploitation with RRdT](https://arxiv.org/abs/1810.03749)*, or this video that briefly explain the concepts:
+<p align="center">
+<a href="https://youtu.be/Cy-kYrzkPvA"><img width="600" height="auto" src="doc/images/rrdt-video-thumbnail.png" alt="RRdT youtube video" /></a>
+</p>
+
+## BibTeX
+If you found the algorithm or the implementations of SBPs framework useful, please cite the following paper.
+```
+@inproceedings{lai_rrdt2019,
+    title={Balancing Global Exploration and Local-connectivity Exploitation with Rapidly-exploring Random disjointed-Trees},
+    author={Lai, Tin and Ramos, Fabio and Francis, Gilad},
+    booktitle={Proceedings of The International Conference on Robotics and Automation (ICRA)},
+    year={2019},
+    organization={IEEE}
+}
+```
---
+++ main.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+"""RRdT* Research
+
+Usage:
+  main.py (rrdt|rrdtinformed|rrt|birrt|informedrrt|prm|particle|likelihood|nearby|mouse) <MAP>
+          [options] [-v|-vv|-vvv]
+  main.py (rrdt|rrdtinformed|rrt|birrt|informedrrt|prm|particle|likelihood|nearby|mouse) <MAP>
+          start <sx> <sy> goal <gx> <gy> [options] [-v|-vv|-vvv]
+  main.py (-h | --help)
+  main.py --version
+
+Arguments:
+  (rrdt|...|...)       Set the sampler to be used by the RRT*.
+  <MAP>                  An image file that represent the map.
+
+General Options:
+  -h --help              Show this screen.
+  --version              Show version.
+  -v --verbose           Display debug message.
+
+Display Options:
+  --always-refresh       Set if the display should refresh on every ticks.
+  -s --scaling=SCALING   Set scaling for display (ratio will be maintained).
+                         [default: 1.5]
+  --hide-sampled-points  Do not display sampled point as red dot on screen.
+  --disable-pygame       Disable pygame display (to enhance performance).
+
+General Sampler Options:
+  --epsilon=EPSILON      Set epsilon value.
+                         [default: 10.0]
+  --radius=RADIUS        Set radius that will connect two nodes together.
+                         [default: 15]
+  --max-number-nodes=MAX_NODES
+                         Set maximum number of nodes
+                         [default: 30000]
+  --ignore-step-size     Ignore step size (i.e. epsilon) when sampling.
+  --goal-bias=BIAS       Probability of biasing goal position.
+                         [default: 0.02]
+
+Random Sampler Options:
+  --random-method=METHOD
+                        Set a random method used to generate the random
+                        numbers. This enables the use of different
+                        quasi-random generators.
+                        Supported methods are:
+                        - pseudo_random (random from numpy)
+                        - saltelli (Saltelli's extension of Sobol sequence)
+                        - sobol_sequence (Sobol sequence generator)
+                        - latin_hypercube (Latin hypercube sampling)
+                        - finite_differences (Derivative-based global
+                                              sensitivity measure)
+                        - fast (Fourier Amplitude Sensitivity Test)
+                        [default: pseudo_random]
+
+Disjoint Sampler Options:
+  --no-restart-when-merge
+                        This flag denotes if the local sampler from disjoint-
+                        tree sampler should restart at a new location when
+                        the disjointed tree branches jointed to an existing
+                        one. The default behaviour is restart as soon as merged
+                        to another existing tree (to encourage exploration).
+                        When this flag is set, it will remain until its energy
+                        is exhausted.
+
+Likelihood/Nearby Sampler Options:
+  --prob-block-size=SIZE
+                        Set the dimension of the discretized block.
+                        [default: 5]
+"""
+
+import os
+import sys
+import logging
+
+from docopt import docopt
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'  # hide pygame prompt
+
+import env
+from helpers import MagicDict
+
+LOGGER = logging.getLogger()
+
+
+def main():
+    args = docopt(__doc__, version='RRT* Research v1.0')
+
+    if args['--verbose'] > 2:
+        LOGGER.setLevel(logging.DEBUG)
+    elif args['--verbose'] > 1:
+        LOGGER.setLevel(logging.INFO)
+    elif args['--verbose'] > 0:
+        LOGGER.setLevel(logging.WARNING)
+    else:
+        LOGGER.setLevel(logging.ERROR)
+    # INFO includes only loading
+    # DEBUG includes all outputs
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setFormatter(logging.Formatter('%(message)s'))
+    # ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    LOGGER.addHandler(ch)
+
+    LOGGER.debug("commandline args: {}".format(args))
+
+    from planners.rrtPlanner import RRTPlanner
+    planner_type = RRTPlanner  # default planner type
+    if args['rrt']:
+        from planners.randomPolicySampler import RandomPolicySampler
+        sampler = RandomPolicySampler(random_method=args['--random-method'])
+    elif args['birrt']:
+        from planners.birrtPlanner import BiRRTSampler, BiRRTPlanner
+        sampler = BiRRTSampler()
+        planner_type = BiRRTPlanner
+    elif args['rrdt']:
+        from planners.rrdtPlanner import RRdTSampler, RRdTPlanner
+        sampler = RRdTSampler(
+            restart_when_merge=not args['--no-restart-when-merge'])
+        planner_type = RRdTPlanner
+    elif args['rrdtinformed']:
+        from planners.rrdtinformed import RRdTInformedSampler, RRdTInformedPlanner
+        sampler = RRdTInformedSampler(
+            restart_when_merge=not args['--no-restart-when-merge'])
+        planner_type = RRdTInformedPlanner
+    elif args['informedrrt']:
+        from planners.informedrrtSampler import InformedRRTSampler
+        sampler = InformedRRTSampler()
+    elif args['prm']:
+        from planners.prmPlanner import PRMSampler, PRMPlanner
+        sampler = PRMSampler()
+        planner_type = PRMPlanner
+    elif args['particle']:
+        from planners.particleFilterSampler import ParticleFilterSampler
+        sampler = ParticleFilterSampler()
+    elif args['likelihood']:
+        from planners.likelihoodPolicySampler import LikelihoodPolicySampler
+        sampler = LikelihoodPolicySampler(
+            prob_block_size=int(args['--prob-block-size']))
+    elif args['nearby']:
+        from planners.nearbyPolicySampler import NearbyPolicySampler
+        sampler = NearbyPolicySampler(
+            prob_block_size=int(args['--prob-block-size']))
+    elif args['mouse']:
+        from planners.mouseSampler import MouseSampler
+        sampler = MouseSampler()
+
+    rrt_options = MagicDict({
+        'showSampledPoint':
+            not args['--hide-sampled-points'],
+        'scaling':
+            float(args['--scaling']),
+        'goalBias':
+            float(args['--goal-bias']),
+        'image':
+            args['<MAP>'],
+        'epsilon':
+            float(args['--epsilon']),
+        'max_number_nodes':
+            int(args['--max-number-nodes']),
+        'radius':
+            float(args['--radius']),
+        'goal_radius':
+            2 / 3 * float(args['--radius']),
+        'ignore_step_size':
+            args['--ignore-step-size'],
+        'always_refresh':
+            args['--always-refresh'],
+        'enable_pygame':
+            not args['--disable-pygame'],
+        'sampler':
+            sampler,
+    })
+    rrtplanner = planner_type(**rrt_options)
+    rrt_options.update({
+        'planner': rrtplanner,
+    })
+
+    if args['start'] and args['goal']:
+        rrt_options.update({
+            'startPt': (float(args['<sx>']), float(args['<sy>'])),
+            'goalPt': (float(args['<gx>']), float(args['<gy>']))
+        })
+
+    rrt = env.Env(**rrt_options)
+    return rrt
+
+
+if __name__ == '__main__':
+    # run if run from commandline
+    rrt = main()
+    try:
+        rrt.run()
+    except Exception as e:
+        LOGGER.error("==============================")
+        LOGGER.exception("Exception occured: {}".format(e))
+        LOGGER.error("==============================")
+        LOGGER.error("Waiting to be exit...")
+        try:
+            rrt.wait_for_exit()
+        except KeyboardInterrupt:
+            pass
